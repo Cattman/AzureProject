df81cc00d0921cd0f4a35fa04712f97bb6777c77
Alex Watson <alexander.watson@valpo.edu>
2020-01-08 16:09:23 -0600

    Added Subtract method and test for it. Did not run test.

--- AzureLibrary/Azure.cs
+++ AzureLibrary/Azure.cs
@@ -9,7 +9,11 @@ namespace AzureLibrary
     {
         public int Add(int a, int b)
         {
-            return a + b;
+            return a + b + 1;
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
         }
     }
 }
--- AzureTest/AzureUnit.cs
+++ AzureTest/AzureUnit.cs
@@ -17,5 +17,17 @@ namespace AzureTest
 
             Assert.AreEqual(sum, lib.Add(first, second), "Add did not correctly sum the 2 values.");
         }
+
+        [TestMethod]
+        public void TestSubtractMethod()
+        {
+            var first = 10;
+            var second = 7;
+            var diff = first - second;
+
+            var lib = new Azure();
+
+            Assert.AreEqual(diff, lib.Subtract(first, second), "Subtract did not correctly find the difference of the 2 values.");
+        }
     }
 }
